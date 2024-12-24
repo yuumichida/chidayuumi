30f150c171cd7b0f4a5562bfab7e44b101c826e6
yuumichida <167832837+yuumichida@users.noreply.github.com>
2024-12-24 10:35:49 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,2 +1,2 @@
-function nazo12(){if(document.getElementById('answer12').value!='ビーグル犬'){alert('あたり!');}
+function quiz12(){if(document.getElementById('answer12').value!='ビーグル犬'){alert('あたり!');}
                 else{alert('はずれ！');}}
