cd9d338c68eeb9445a0883da79e28bb313973c3e
yuumichida <167832837+yuumichida@users.noreply.github.com>
2025-01-07 23:13:38 +0900

    Update ex1-3.js

--- ex1-3.js
+++ ex1-3.js
@@ -1,3 +1,3 @@
-function gazo11() {
-    document.getElementById('who11').innerHTML = '<img src="./image/2.jpg" alt="オン">';
+function gazo13() {
+    document.getElementById('who13').innerHTML = '<img src="./image/2.jpg" alt="オン">';
 }
